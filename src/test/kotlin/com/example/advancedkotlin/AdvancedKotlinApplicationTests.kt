56575171ad8a375b50bc1b94c5d8a79c4415e9ea
cherhy <ekxk1234@naver.com>
2024-01-23 22:08:06 +0900

    create project

---
+++ src/test/kotlin/com/example/advancedkotlin/AdvancedKotlinApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.example.advancedkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AdvancedKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
