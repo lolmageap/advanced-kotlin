786c3790e453f9ce9a99dce0e394972ef13367d2
lolmageap <ekxk1234@naver.com>
2024-01-24 23:33:52 +0900

    inline reified function

---
+++ src/main/kotlin/generic/TypeErase.kt
@@ -0,0 +1,21 @@
+package generic
+
+fun main() {
+    val numbers = listOf(1, 2.0, 3.1f, 4L)
+
+    val long = numbers.filterIsInstance<Long>()
+    require(long.size == 1)
+}
+
+inline fun <reified T> T.toSuperString() {
+    println("${T::class.java.name}: $this")
+}
+
+inline fun <reified T> List<*>.hasAnyInstanceOf(): Boolean {
+    return this.any { it is T }
+}
+
+class TypeErase {
+
+
+}
