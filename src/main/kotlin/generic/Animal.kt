ce5fb288bdedf80463f071959e284321d3a2bf03
lolmageap <ekxk1234@naver.com>
2024-01-24 23:32:23 +0900

    create bird extend Comparable

--- src/main/kotlin/generic/Animal.kt
+++ src/main/kotlin/generic/Animal.kt
@@ -10,3 +10,16 @@ abstract class Fish(name: String): Animal(name)
 class GoldFish(name: String): Fish(name)
 
 class Carp(name: String): Fish(name)
+
+abstract class Bird(
+    name: String,
+    private val size: Int,
+): Animal(name), Comparable<Bird> {
+    override fun compareTo(other: Bird): Int {
+        return size.compareTo(other.size)
+    }
+}
+
+class Sparrow: Bird("참새", 100)
+
+class Eagle: Bird("독수리", 500)
