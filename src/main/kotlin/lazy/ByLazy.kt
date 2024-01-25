d0e1663edc5dc2dc45b6f242e3fb209ceb8ae4b6
lolmageap <ekxk1234@naver.com>
2024-01-25 22:10:22 +0900

    add delegate

--- src/main/kotlin/lazy/ByLazy.kt
+++ src/main/kotlin/lazy/ByLazy.kt
@@ -1,12 +1,25 @@
 package lazy
 
+import kotlin.properties.Delegates.observable
+import kotlin.properties.Delegates.vetoable
 import kotlin.reflect.KProperty
+
+fun main() {
+    val person = Person5()
+    person.age = 30
+    person.weight = 40
+    require(person.weight == 40)
+}
 
 class Person3 {
     val name: String by lazy {
         Thread.sleep(2_000)
         "김병기"
     }
+
+    @Deprecated("사용하지 않음", ReplaceWith("age"))
+    val num: Int = 0
+    val age: Int by this::num
 }
 
 // Person3 클래스 와 Person4 클래스 는 동일한 기능을 수행 한다.
@@ -19,6 +32,20 @@ class Person4 {
 
     val name: String
         get() = delegateProperty.value
+}
+
+class Person5 {
+    var age: Int by observable(20) { _, oldValue, newValue ->
+        if (oldValue != newValue) {
+            println("이전 값: $oldValue")
+            println("새로운 값: $newValue")
+        }
+    }
+
+    // weight 값이 old value 보다 크면 true 를 반환 하고, 그렇지 않으면 false 를 반환 한다.
+    var weight: Int by vetoable(50) { _, oldValue, newValue ->
+        newValue > oldValue
+    }
 }
 
 class LazyInitProperty<T>(val init: () -> T) {
