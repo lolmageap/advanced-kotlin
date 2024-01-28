8a8e29783edbbbbfda7cee6500d2b690465deded
lolmageap <ekxk1234@naver.com>
2024-01-28 20:30:48 +0900

    study coroutine basic

--- src/main/kotlin/coroutine/RunBlocking.kt
+++ src/main/kotlin/coroutine/RunBlocking.kt
@@ -1,17 +1,64 @@
 package coroutine
 
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.yield
+import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
 
+fun main() {
+    example4()
+}
 
-fun main() = runBlocking {
-    printWithThreadName("START")
+fun example4() =
+    measureTimeMillis {
+        runBlocking {
+            val job1 = launch {
+                delay(1_000)
+                printWithThreadName("JOB 1")
+            }
 
-    launch {
-        newFunction()
+            // job 에 join 을 걸면 해당 job 이 끝날 때까지 다음 job 이 기다린 다.
+            job1.join()
+
+            val job2 = launch {
+                delay(1_000)
+                printWithThreadName("JOB 2")
+            }
+        }
+    }.let {
+        printWithThreadName("TIME: $it")
     }
-    yield()
+
+fun example3() = runBlocking {
+    val job = launch {
+        (1..5).forEach {
+            printWithThreadName(it)
+            delay(500)
+        }
+    }
+
+    delay(1_000)
+    job.cancel()
+}
+
+fun example2() = runBlocking {
+    val job = launch(start = CoroutineStart.LAZY) {
+        printWithThreadName("LAUNCH START")
+    }
+
+    delay(1_000)
+    job.start()
+    printWithThreadName("END")
+}
+
+fun example1() {
+    runBlocking {
+        printWithThreadName("START")
+
+        launch {
+            printWithThreadName("LAUNCH START")
+            delay(2_000)
+            printWithThreadName("LAUNCH END")
+        }
+    }
     printWithThreadName("END")
 }
 
